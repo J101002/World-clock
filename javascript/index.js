81e478586304ae3c5a34f4ebdb4f8be974d9713a
J101002 <jennifersalto143@gmail.com>
2023-12-05 11:58:37 -0800

    Added link to homepage

--- javascript/index.js
+++ javascript/index.js
@@ -42,6 +42,7 @@ function updateCity(event) {
           </div>
           <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
         </div>
+        <a href="index.html">All cities</a>
     `;
 }
 
